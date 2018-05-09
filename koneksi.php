9785d51f8ea536ffe14d3292619e5a313acc8283
Fredy Nur Apriyanto <mail.fredyna@gmail.com>
2018-05-09 16:25:40 +0700

    update travis

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
     class Koneksi{
         private $server = "localhost";
-        private $username = "root";
-        private $password = "";
-        private $db = "phb_kelas_c";
+        private $username = "id4994133_fredynur";
+        private $password = "Bismillah";
+        private $db = "id4994133_stok_barang";
 
         function getKoneksi(){
             return new mysqli($this->server, $this->username, $this->password, $this->db);
